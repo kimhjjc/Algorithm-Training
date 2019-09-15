a7fef69af10d0921b365228adc04a5c06e7ba87b
kimhjjc <49774180+kimhjjc@users.noreply.github.com>
2019-09-15 23:44:25 +0900

    Add files via upload

---
+++ 1, 2, 3 더하기/1, 2, 3 더하기/1, 2, 3 더하기.cpp
@@ -0,0 +1,59 @@
+﻿// 1, 2, 3 더하기.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+
+using namespace std;
+// 정수 n이 주어졌을 때, n을 1, 2, 3의 합으로 나타내는 방법의 수를 구하는 프로그램을 작성하시오.
+//첫째 줄에 테스트 케이스의 개수 T가 주어진다.
+//각 테스트 케이스는 한 줄로 이루어져 있고, 정수 n이 주어진다. n은 양수이며 11보다 작다.
+int number_of_case(int number);
+
+int main()
+{
+	int count = 0;
+
+	cin >> count;
+
+	for (int i = 0; i < count; i++)
+	{
+		int check = 0;
+
+		cin >> check;
+
+		cout << number_of_case(check) << endl;
+
+	}
+}
+
+int number_of_case(int number)
+{
+	static int cache[10];
+
+	if (cache[number] != 0)
+		return cache[number];
+
+	if (number >= 4)
+	{
+		cache[number] = number_of_case(number - 1) + number_of_case(number - 2) + number_of_case(number - 3);
+		return cache[number];
+	}
+	else if (number == 3)
+		return  4;
+	else if (number == 2)
+		return  2;
+	else if (number == 1)
+		return  1;
+	
+}
+
+// 프로그램 실행: <Ctrl+F5> 또는 [디버그] > [디버깅하지 않고 시작] 메뉴
+// 프로그램 디버그: <F5> 키 또는 [디버그] > [디버깅 시작] 메뉴
+
+// 시작을 위한 팁: 
+//   1. [솔루션 탐색기] 창을 사용하여 파일을 추가/관리합니다.
+//   2. [팀 탐색기] 창을 사용하여 소스 제어에 연결합니다.
+//   3. [출력] 창을 사용하여 빌드 출력 및 기타 메시지를 확인합니다.
+//   4. [오류 목록] 창을 사용하여 오류를 봅니다.
+//   5. [프로젝트] > [새 항목 추가]로 이동하여 새 코드 파일을 만들거나, [프로젝트] > [기존 항목 추가]로 이동하여 기존 코드 파일을 프로젝트에 추가합니다.
+//   6. 나중에 이 프로젝트를 다시 열려면 [파일] > [열기] > [프로젝트]로 이동하고 .sln 파일을 선택합니다.
